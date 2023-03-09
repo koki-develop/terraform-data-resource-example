0d3f430056c19807157aa4c3d069234c2601f368
koki-develop <kou.pg.0131@gmail.com>
2023-03-09 16:55:04 +0900

    null_resource を代替する terraform_data を作成

--- terraform_data.tf
+++ terraform_data.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "main" {
-  // インスタンスの ID が変わった ( インスタンスが作り直された ) ときにコマンドを実行する例
+  # インスタンスの ID が変わった ( インスタンスが作り直された ) ときにコマンドを実行する例
   triggers = {
     instance_id = aws_instance.main.id
   }
@@ -7,5 +7,17 @@ resource "null_resource" "main" {
   provisioner "local-exec" {
     # 実行するコマンド
     command = "echo null resource"
+  }
+}
+
+resource "terraform_data" "like_null_resource" {
+  # インスタンスの ID が変わった ( インスタンスが作り直された ) ときにコマンドを実行する例
+  triggers_replace = [
+    aws_instance.main.id
+  ]
+
+  # 実行するコマンド
+  provisioner "local-exec" {
+    command = "echo terraform data"
   }
 }
