1b135b5f0501f3d89d8451ba7e58cdb9f4498660
koki-develop <kou.pg.0131@gmail.com>
2023-03-09 16:47:51 +0900

    22 port 開けといた

--- ec2.tf
+++ ec2.tf
@@ -40,7 +40,27 @@ data "aws_ami" "amazon_linux2" {
 }
 
 resource "aws_instance" "main" {
-  ami           = data.aws_ami.amazon_linux2.image_id
-  subnet_id     = data.aws_subnet.main.id
-  instance_type = "t2.micro"
+  ami                    = data.aws_ami.amazon_linux2.image_id
+  subnet_id              = data.aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  instance_type          = "t2.micro"
+}
+
+resource "aws_security_group" "instance" {
+  name   = "example"
+  vpc_id = data.aws_subnet.main.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
